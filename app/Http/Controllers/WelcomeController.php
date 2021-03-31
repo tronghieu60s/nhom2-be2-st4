3d05f4f812acca3e5b782abd51b36b07b48be66c
Student <Student@tdc.edu.vn>
2021-03-31 10:35:34 +0700

    update login middleware

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -10,4 +10,12 @@ class WelcomeController extends Controller
     public function index () {
         return view('welcome');
     }
+
+    public function admin () {
+        return "Hello Admin!";
+    }
+
+    public function login () {
+        return view('login');
+    }
 }   
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'WelcomeController@index');
-// Route::get('/products', 'WelcomeController@index');
 Route::get('/products/{id?}', 'ProductsController@index');
+Route::get('/admin', 'WelcomeController@admin')->middleware('login');
+Route::get('/login', 'WelcomeController@login');
