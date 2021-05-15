393319358dea23f53088562d0ed867a394d08897
Nguyễn Ngọc Trường <truongth2904@gmail.com>
2021-05-15 21:35:21 +0700

    update check order, delete order

--- app/Order.php
+++ app/Order.php
@@ -15,6 +15,6 @@ class Order extends Model
 
     public function user()
     {
-        return $this->belongsTo('App\User', 'user_id ');
+        return $this->belongsTo('App\User', 'user_id');
     }
 }
